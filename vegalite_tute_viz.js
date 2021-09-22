ab18426758f771ca14cc8ad5def06847b99e9fa5
Lim Tzeyi <limtzeyi@gmail.com>
2021-09-23 00:46:11 +0800

    removed useless data

--- vegalite_tute_viz.js
+++
@@ -1,4 +0,0 @@
-var vg_2 = "vegalite_tute_viz.vg.json";
-vegaEmbed("#flow_map", vg_2).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
